efc32a0504bdb8eb4ebc5aa3133595f598c4eea1
Jirayu Suebwongphanudet <jirayu.sueb@gmail.com>
2024-03-04 22:26:31 +0700

    📦 feat(package.json): add new dependencies for form handling and validation 📝 feat(sign-in/page.tsx): create new sign-in page with form validation 🎨 feat(form.tsx, label.tsx): add new form and label components for UI enhancement
    The new dependencies added to package.json are for form handling and validation. The '@hookform/resolvers' and 'zod' are used for form validation, 'react-hook-form' for form state management, and '@radix-ui/react-label' for UI enhancement.
    
    A new sign-in page is created with form validation. The form validation is done using 'zod' and 'react-hook-form'. The form includes fields for email and password.
    
    New form and label components are added for UI enhancement. The form component uses 'react-hook-form' for form state management and includes various form-related components such as FormField, FormLabel, FormControl, FormDescription, and FormMessage. The label component uses '@radix-ui/react-label' for UI enhancement.

---
+++ src/app/[locale]/auth/sign-in/page.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export default function SignInPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = () => {
+    console.log('submit');
+  };
+
+  return (
+    <div className="fixed inset-0 z-10 mx-auto flex bg-white">
+      <div className="mx-auto flex h-screen w-screen max-w-md items-center justify-center">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-2"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Enter your email..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter your password..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="ml-auto mt-4 w-full" type="submit">
+              Sign In
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
